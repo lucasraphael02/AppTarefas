92f63f679d0209c9c0c40c047520f63f68de1b0f
lucasraphael02 <lucasraphael@aluno.ufrb.edu.br>
2026-01-12 15:14:32 -0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setScene(new Scene(new Label("Olá JavaFX com JDK 21!"), 300, 200));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
